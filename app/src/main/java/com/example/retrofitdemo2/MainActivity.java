b73dc412e910445916f46d19ca0fc4f6af58220c
nipunm1 <nipunmiglani.96@gmail.com>
2019-07-10 01:23:12 +0530

    First Commit

--- app/src/main/java/com/example/retrofitdemo2/MainActivity.java
+++ app/src/main/java/com/example/retrofitdemo2/MainActivity.java
@@ -6,8 +6,10 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.retrofitdemo2.adapter.MyRecyclerAdapter;
 import com.example.retrofitdemo2.api.RetroFitClient;
 import com.example.retrofitdemo2.model.MyGit;
@@ -19,17 +21,21 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
-//TextView textView;
-RecyclerView recyclerView;
+TextView textView,textView2;
+ImageView imageView;
+//RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //textView = findViewById(R.id.text);
-        recyclerView = findViewById(R.id.recyclerview);
-        getMyGitAccount();
+        textView = findViewById(R.id.text);
+        textView2 = findViewById(R.id.text2);
+        imageView = findViewById(R.id.image);
+        //recyclerView = findViewById(R.id.recyclerview);
+        //getMyGitAccount();
+        getMyGitAccount2();
     }
-    private void getMyGitAccount(){
+    /*private void getMyGitAccount(){
         RetroFitClient.api.getMyGit("nipunm1").enqueue(new Callback<List<MyGit>>() {
             @Override
             public void onResponse(Call<List<MyGit>> call, Response<List<MyGit>> response) {
@@ -46,6 +52,27 @@ RecyclerView recyclerView;
             public void onFailure(Call<List<MyGit>> call, Throwable t) {
                 //Log.d("error : ", t.getMessage());
                 //textView.setText(t.getMessage());
+            }
+        });
+    }*/
+    private void getMyGitAccount2(){
+        RetroFitClient.api.getMyGit2("nipunm1").enqueue(new Callback<MyGit>() {
+            @Override
+            public void onResponse(Call<MyGit> call, Response<MyGit> response) {
+                //Log.d("response", response.toString());
+                MyGit myGit = response.body();
+                String name = myGit.getLogin();
+                int id = myGit.getId();
+                textView.setText(name);
+                textView2.setText(id+"");
+                Glide.with(MainActivity.this).load(myGit.getAvatarUrl()).into(imageView);
+
+                //textView.setText(response.toString());
+            }
+
+            @Override
+            public void onFailure(Call<MyGit> call, Throwable t) {
+                Log.d("error", t.getMessage());
             }
         });
     }
--- app/src/main/java/com/example/retrofitdemo2/api/MyApi.java
+++ app/src/main/java/com/example/retrofitdemo2/api/MyApi.java
@@ -11,4 +11,6 @@ import retrofit2.http.Path;
 public interface MyApi {
 @GET("/users/{user}/following")
 Call<List<MyGit>> getMyGit(@Path("user")String user);
+@GET("/users/{user}")
+Call<MyGit> getMyGit2(@Path("user")String user);
 }
